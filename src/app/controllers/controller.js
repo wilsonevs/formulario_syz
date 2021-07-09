85ced41f88eda4e46474c3a15e6700a964e5434a
wilsonevs <wilsonevs@gmail.com>
2021-07-09 08:42:57 -0500

    Ingreso del archivo de prueba

--- src/app/controllers/controller.js
+++ src/app/controllers/controller.js
@@ -1,5 +1,4 @@
-
-const {} = require("../../config/connectionDb");
+const pool = require("../../config/connectionDb");
 
 const getIndex = (req, res) => {
     res.render("formulario.ejs");
@@ -7,28 +6,58 @@ const getIndex = (req, res) => {
 
 
 const postIndex = async (req, res) => {
-    const { firstname, lastname, telefono, celular, email, empresa, observacion } = req.body;
-    console.log(req.body);
-    pool.query("INSERT INTO contactanos VALUES ($1, $2, $3, $4, $5, $6, $7)",[firstname, lastname, telefono, celular, email, empresa,observacion],
-      async (error, results) => {
-        if (error) {
-          console.log("Que error tengo" + error);
-        } else {
-          res.render("index.ejs", {
-            login,
-            alert: true,
-            alertTitle: "Enviado",
-            alertMessage: "¡Te estaremos contactando!",
-            alertIcon: "success",
-            showConfirmButton: false,
-            timer: false,
-            ruta: "/",
-          });
-        }
+  const { firstname, lastname, telefono, celular, email, empresa, observacion } = req.body;
+  const datos="INSERT INTO contactanos (firstname, lastname, telefono, celular, email, empresa, observacion) VALUES ($1, $2, $3, $4, $5, $6, $7)";
+  const values= [firstname, lastname, telefono, celular, email, empresa, observacion ];
+  console.log(req.body);
+  try {
+    pool.query(datos, values, (err, res) => {
+        
+      if (error) {
+        console.log("Que error tengo" + error);
+      } else {
+        res.render("index.ejs", {
+          alert: true,
+          alertTitle: "Enviado",
+          alertMessage: "¡Te estaremos contactando!",
+          alertIcon: "success",
+          showConfirmButton: false,
+          timer: false,
+          ruta: "/",
+        });
       }
-    );
-  };
+
+      });
+
+  } catch (error) {
+    console.log("El ERROR ES: "+error);
+    return res.status(500).json('Internal server error');
+  }
+};
+
+
+const getW = async (req, res) =>{
+  try {
+    const querys= "SELECT * FROM nombres";
+    pool.query(querys, (error, results) =>{
+      res.render("prueba.ejs", {
+        datos: results,
+      })
+    })
+
+  } catch (error) {
+    console.log("El ERROR ES: "+error);
+    
+    // return res.status(500).json('Internal server error');
+  }
+
+}
 
 
 
-module.exports = {getIndex, postIndex}
+
+module.exports = {
+  getIndex, 
+  postIndex, 
+  getW,
+}
--- src/app/routers/router.js
+++ src/app/routers/router.js
@@ -1,10 +1,18 @@
 // const conexion = require("../../config/connectionDb.js");
 const {app} = require("express");
-const {getIndex, postIndex} = require("../controllers/controller");
+const {
+    getIndex, 
+    postIndex,
+
+    getW,   
+} = require("../controllers/controller");
 
 
 
 module.exports = (app) => {
     app.get("/", getIndex);
     app.post("/", postIndex);
+
+    app.get("/w", getW);
+
 }

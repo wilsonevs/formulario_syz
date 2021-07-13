123c1bab667161815454a4d6c568dd5dc3935bce
wilsonevs <wilsonevs@gmail.com>
2021-07-13 01:30:11 -0500

    Correción del llamado values en index.ejs

--- src/app/routers/router.js
+++ src/app/routers/router.js
@@ -14,13 +14,13 @@ module.exports = (app) => {
     app.get("/", getIndex);
     app.post("/", [
         // validadores back-end
-        body('firstname','Ingresar un nombre valido').exists().isLength({min:3}).trim().matches('^\w+\s?\w+?\s?$'),
-        body('lastname', 'Ingrese apellido completo').exists().isLength({min:3}).trim().matches('^\w+\s?\w+?\s?$'),
-        body('telefono', 'Ingrese un teléfono valido').exists().isNumeric().trim().matches('^[+|\d]?\s?\d{1,10}?\s?\d{1,10}?\s?\d{1,10}\s?\d{1,10}\s?$'),
-        body('celular', 'Ingrese un celular valido').exists().isNumeric().trim().matches('^[+|\d]?\s?\d{1,10}?\s?\d{1,10}?\s?\d{1,10}\s?\d{1,10}\s?$'),
-        body('email', 'Ingrese un email valido').exists().isEmail().trim().matches('^([a-zA-Z0-9_\.\+-]+)@([\da-zA-Z\.-]+)\.([a-zA-Z\.]{2,6})$'),
-        body('empresa', 'Ingrese nombre de la empresa').exists().isLength({min:3}).trim().matches('\s?([\s._+-]?\w+)'),
         body('observacion', 'Deja una observación. ¡Por favor!').exists().isLength().trim(),
+        body('empresa', 'Ingrese nombre de la empresa').exists().isLength({min:3}).trim().matches('\s?([\s._+-]?\w+)'),
+        body('email', 'Ingrese un email valido').exists().isEmail().trim().matches('^([a-zA-Z0-9_\.\+-]+)@([\da-zA-Z\.-]+)\.([a-zA-Z\.]{2,6})$'),
+        body('celular', 'Ingrese un celular valido').exists().isNumeric().trim().matches('^[+|\d]?\s?\d{1,10}?\s?\d{1,10}?\s?\d{1,10}\s?\d{1,10}\s?$'),
+        body('telefono', 'Ingrese un teléfono valido').exists().isNumeric().trim().matches('^[+|\d]?\s?\d{1,10}?\s?\d{1,10}?\s?\d{1,10}\s?\d{1,10}\s?$'),
+        body('lastname', 'Ingrese apellido completo').exists().isLength({min:3}).trim().matches('^\w+\s?\w+?\s?$'),
+        body('firstname','Ingresar un nombre valido').exists().isLength({min:3}).trim().matches('^\w+\s?\w+?\s?$'),
     ], index_Validacion_post);
 
     app.get("/prueba", getW);
